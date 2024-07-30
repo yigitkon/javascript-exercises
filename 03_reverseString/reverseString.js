3a10e79587e0c1ea734b9ced9a3d916bba2f6be4
yigitkon <yigitkonuskan@gmail.com>
2024-07-30 11:55:52 +0300

    03_reverseString exercise is done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    reverse_string_array = []
+    for (let i=string.length-1; i>=0;i--){
+        reverse_string_array.push(string[i])
+    }
+    return reverse_string_array.join("")
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+
