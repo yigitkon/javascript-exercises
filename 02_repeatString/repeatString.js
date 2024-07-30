e3b589b035c71ce1e88705e4096103ef10e8863a
yigitkon <yigitkonuskan@gmail.com>
2024-07-30 11:41:43 +0300

    02_repeatString exercise is done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if (num < 0){
+        return 'ERROR'
+    }else if(num == 0){
+        return ""
+    }else{
+        let final_string = ""
+        for (let i=0; i<num; i ++){
+            final_string += string
+        }
+        return final_string
+    }
+    
 
 };
 
