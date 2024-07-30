c0df0be551cf287e2bc9fe116a0f81ee2f6f1388
yigitkon <yigitkonuskan@gmail.com>
2024-07-30 13:14:10 +0300

    07_tempConversion exercise is done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  let celcius = 5 * (temperature - 32) / 9
+  celcius = Math.round(celcius * 10)/ 10
+  return celcius 
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  let fahrenheit = (9 / 5) * temperature + 32
+  fahrenheit = Math.round(fahrenheit * 10) / 10
+  return fahrenheit
 };
 
 // Do not edit below this line
@@ -9,3 +15,4 @@ module.exports = {
   convertToCelsius,
   convertToFahrenheit
 };
+
