06cc363d4e5df268281b9c8748e6b19dbc46d90b
yigitkon <yigitkonuskan@gmail.com>
2024-07-30 13:05:58 +0300

    06_leapYears exercise is done

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,17 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year % 400 == 0){
+        return true
+    }else if (year % 100 == 0){
+        return false
+    }else if (year % 4 == 0){
+        return true
+    }else{
+        return false
+    }
 };
 
 // Do not edit below this line
 module.exports = leapYears;
+
+
+console.log(leapYears(16))
