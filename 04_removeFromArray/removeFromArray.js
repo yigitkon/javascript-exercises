53709aba7a5991f726932638f97e001223fe58f9
yigitkon <yigitkonuskan@gmail.com>
2024-07-30 12:36:14 +0300

    04_removeFromArray exercise is done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
 const removeFromArray = function() {
-};
+    const array = arguments[0]
+    const deletedIndices = []
+    for (let i=1;i<arguments.length;i++){
+        for (let j=0;j<array.length;j++){
+            if (array[j] === arguments[i]){
+                deletedIndices.push(j);
+            }
+        }
+    }
+    deletedIndices.sort();
+    for (let i=deletedIndices.length-1; i>=0; i--){
+        array.splice(deletedIndices[i],1)
+    }
+    return array
+};  
 
 // Do not edit below this line
 module.exports = removeFromArray;
